896a4704af144d7b48c17fcb0ffd5a0499b23e16
Maria Alexandre <128512244+MariaZecaKitty@users.noreply.github.com>
2023-04-20 17:18:51 +0100

    Codigo

--- sketch.js
+++ sketch.js
@@ -1,22 +1,36 @@
-let espLarg = 20;
-let espAltura = 20;
+let espLarg = 5;
+let espAltura = 5;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(0);
-  stroke(255);
-  strokeWeight(2);
-  noFill();
-  background(0);
+  background(255);
 }
 
 function draw() {
-  
   for (let i = 1; i < espLarg; i++) {
     for (let j = 1; j < espAltura; j++) {
       push();
-      translate(i * width / espLarg,j * height / espAltura)
-      circle(0, 0, height / espAltura);
+      translate((i * width) / espLarg, (j * height) / espAltura);
+      scale (0.50);
+	posX = 0;
+	posY = 0;
+	noFill();
+	stroke(0);
+	strokeWeight(5);
+
+    fill(255, 215, 0)
+	circle(posX, posY, height/3);
+
+    fill(255)
+	circle(posX - height/3/4, posY - height/18, height/12);
+	circle(posX + height/3/4, posY - height/18, height/12);
+
+    fill(0)
+    circle(posX - height/3/4, posY - height/18, height/20);
+	circle(posX + height/3/4, posY - height/18, height/20);
+
+    fill(255,0,0)
+    circle(posX + height/3/25, posY - height/70, height/30);
       pop();
     }
   }
